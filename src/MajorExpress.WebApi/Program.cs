9d28ea9db3280f4f9dd2eb04df4d363d564ed498
Vladislav Medvedev <v.medvedev@cybertradingmachines.com>
2023-09-20 00:52:03 +0300

    Initial migration

--- src/MajorExpress.WebApi/Program.cs
+++ src/MajorExpress.WebApi/Program.cs
@@ -1,24 +1,25 @@
+using MajorExpress.Infrastructure;
+using MajorExpress.Infrastructure.Common.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger()
+       .UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+await app.MigrateDatabase(app.Lifetime.ApplicationStopping);
 
-app.UseAuthorization();
+app.UseHttpsRedirection()
+   .UseAuthorization();
 
 app.MapControllers();
 
