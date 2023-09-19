16669007fad940c35f415b47ec56079b91c67911
Vladislav Medvedev <v.medvedev@cybertradingmachines.com>
2023-09-19 20:27:14 +0300

    Add IDeliveryRequestService

---
+++ src/MajorExpress.Application/Common/Interfaces/IDeliveryRequestService.cs
@@ -0,0 +1,21 @@
+﻿using MajorExpress.Domain.Entities;
+
+namespace MajorExpress.Application.Common.Interfaces;
+
+public interface IDeliveryRequestService
+{
+    Task<Guid> CreateAsync(
+        string departureAddress,
+        DateTime departureTime,
+        string destinationAddress,
+        DateTime destinationTime,
+        Guid clientId,
+        Guid cargoId,
+        CancellationToken cancellationToken);
+
+    Task<Guid> AssignAsync(Guid deliveryRequestId, Guid assigneeId, CancellationToken cancellationToken);
+
+    Task<Guid> CancelAsync(Guid deliveryRequestId, string comment, CancellationToken cancellationToken);
+
+    Task<IReadOnlyCollection<DeliveryRequest>> SearchAsync(string tag, CancellationToken cancellationToken);
+}
